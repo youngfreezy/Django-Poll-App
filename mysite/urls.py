85ff46fcf6f2902965ae869a4838568fdd6b2631
Fareez Ahmed <Fareez.Ahmed@gmail.com>
2015-04-16 13:38:15 -0400

    configured root route, fixed test case

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
+	url(r'^$', lambda x: HttpResponseRedirect('/polls/')),
 	url(r'^polls/', include('polls.urls', namespace="polls")),
 	url(r'^admin/', include(admin.site.urls)),
 ]
